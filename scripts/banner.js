3bce8a58b8fd826352770ee794c7a8452ffca262
fanwei <fanwei@gaoding.com>
2021-11-09 23:05:23 +0800

    chore: correct the banner

--- scripts/banner.js
+++ scripts/banner.js
@@ -4,7 +4,7 @@ module.exports = `
 /**
  * ${pkg.name}@${pkg.version}
  * ${pkg.description}
- * @author ${pkg.author}
+ * @author ${pkg.author.name} <${pkg.author.email}>
  * @license ${pkg.license}
  * @see {@link ${pkg.homepage}}
  */`
